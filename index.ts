6e6ec8e9a0b3d0021f4d58d601278409804bbd71
anderson <nobreanderson@hotmail.com>
2024-12-17 18:37:01 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+// Função de multiplicação
+    function multiplicar(a: number, b: number): number {
+        return a * b;
+    }
+    
+    // Função de saudação
+    function saudar(nome: string): string {
+        return "Olá " + nome;
+    }
+    
+    // Exemplos de uso
+    const resultadoMultiplicacao = multiplicar(5, 3); // 15
+    console.log(resultadoMultiplicacao);
+    
+    const saudacao = saudar("Anderson"); // "Olá Anderson"
+    console.log(saudacao);
+    
